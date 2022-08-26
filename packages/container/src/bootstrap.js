3c90c26da98d9f1f2f098841fa3c7e0668ed3fbc
Bruno Verussa <b.verussa@gmail.com>
2022-08-26 15:05:25 -0700

    Test deploy

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Render App component
+// Render App component into the root ID
 ReactDOM.render(<App />, document.querySelector("#root"));
