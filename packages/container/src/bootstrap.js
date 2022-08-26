65b1e9baedb7ceda1f079819af99341d45a90ce4
Bruno Verussa <b.verussa@gmail.com>
2022-08-26 13:55:29 -0700

    Test invalidation step

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Render App
+// Render App component
 ReactDOM.render(<App />, document.querySelector("#root"));
